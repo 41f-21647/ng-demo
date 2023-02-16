244b3f43686944730b2fa09e12f1bc5bcfdc279a
Jonathan Martel <jonathmartel@gmail.com>
2023-02-16 14:14:30 -0500

    début ou fin du dernier cours...

---
+++ src/app/entete/entete.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entete',
+  templateUrl: './entete.component.html',
+  styleUrls: ['./entete.component.scss']
+})
+
+export class EnteteComponent {
+  titre:String = "Administration";
+  
+
+
+}
